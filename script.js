c8e80cd7ad9ad1584d6802aa40a71bded5b512ec
i.medulashvili <i.medulashvili@Airimedlashvili.netis.cc>
2024-04-04 00:18:07 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+    
+
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+
+  function zoomImage(img) {
+    img.classList.toggle("zoomed");
+}
+
+function downloadPDF() {
+  var pdfUrl = 'portfoliopdf.pdf';
+  
+  var a = document.createElement('a');
+  a.href = pdfUrl;
+  a.download = 'downloaded-file.pdf';
+  
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
+
