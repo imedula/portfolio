015c07792dc40c21e13bccffdb5b2f08da91a447
iu.medulashvili@gmail.com <iu.medulashvili@gmail.com>
2024-06-19 17:20:52 +0400

    some changes in descriptions

--- script.js
+++ script.js
@@ -5,20 +5,17 @@ let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
   sections.forEach(sec => {
-      let top = window.scrollY;
-      let offset = sec.offsetTop - 150;
-      let height = sec.offsetHeight;
-      let id = sec.getAttribute('id');
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-      if (top >= offset && top < offset + height) {
-          navLinks.forEach(link => {
-              link.classList.remove('active');
-          });
-          let navLink = document.querySelector('header nav a[href*=' + id + ']');
-          if (navLink) {
-              navLink.classList.add('active');
-          }
-      }
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+      });
+      document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
+    }
   });
 };
 
@@ -26,7 +23,7 @@ window.onscroll = () => {
   
 menuIcon.onclick = () => {
       menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('navbar');
+    navbar.classList.toggle('active');
 };
     
 
