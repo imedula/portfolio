98389e148b3e9036f30b8496988983e9564f0324
iu.medulashvili@gmail.com <iu.medulashvili@gmail.com>
2024-10-01 11:47:48 +0400

    color changes

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
   sections.forEach(sec => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach(links => {
-        links.classList.remove('active');
-      });
-      document.querySelector('header nav a[href="#' + id + '"]').classList.add('active');
-    }
-  });
+      if(top >= offset && top < offset + height) {
+          navLinks.forEach(links => {
+              links.classList.remove('active');
+              document.querySelector('header .nav_container nav a [href*=' + id + ']').classList.add ('active');
+          })
+      }
+  })
 };
 
 
   
 menuIcon.onclick = () => {
       menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('navbar');
+    navbar.classList.toggle('active');
 };
     
 
@@ -60,3 +60,5 @@ function downloadPDF() {
   document.body.removeChild(a);
 }
 
+
+  
